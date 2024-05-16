9c278c8e65fd4e44d6cf17493711ee08a66246e9
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-16 22:33:51 +0530

    created model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to mongo db")
+})
 const app = express();
+
+
+//mongo db pass- 
+//adityakr4005
+//mXTHO5s77cLgEc4d
 
 app.listen(3000, () => {
     console.log("server running on 3000");
