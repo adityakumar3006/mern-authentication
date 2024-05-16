b1de59d0eabb937837b8d57c4af2ae793fa403fd
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-16 22:46:59 +0530

    create test api

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+// it is a function 
+export const test = (req, res) => {
+    res.json({
+        message: "Api is working"
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.route.js"
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to mongo db")
@@ -16,3 +16,11 @@ const app = express();
 app.listen(3000, () => {
     console.log("server running on 3000");
 })
+
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Api is working",
+//     })
+// })
+app.use("/api/user", userRoutes);
