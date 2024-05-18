13d8d4b8eeb763e0e0303f744b0f5351125b78d9
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-18 09:54:46 +0530

    complete sign in page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
     // console.log(req.body);
 
@@ -14,10 +16,33 @@ export const signup = async (req, res, next) => {
     }
     catch (error) {
         // res.status(500).json(error.message)
-        // next ia a kind of miidle ware instead of writing res.json (500).json(error.message)
+        // next ia a kind of miidle ware instead of writing res.json (500).json(error.message) multiple time it directs to the next function
+
         next(error);
         //or
         // next(errorHandler(300,"something went wrong"));  // custom error 
 
     }
 };
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser)
+            return next(new errorHandler(404, "User not found"))
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(new errorHandler(401, "wrong credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc
+        const expireDate = new Date(Date.now() + 3600000);// 1 hour
+        res.cookie("access_token", token, { httpOnly: true, expires: expireDate }).status(200).json(rest);
+
+
+
+
+    }
+    catch (error) {
+        next(error);
+    }
+
+}
